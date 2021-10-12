268452ac19c3c4e38226da2cb2196ecad62ee5a3
Elefly13 <elymara13@gmail.com>
2021-10-12 16:00:23 +0200

    Struttura card

--- index.php
+++ index.php
@@ -4,13 +4,38 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+
     <link rel="stylesheet" href="css/style.css">
     <title>Document</title>
 </head>
 <body>
 <?php 
     include __DIR__ .'/partial/database.php';
+    include __DIR__ .'/partial/function.php';
 ?>
+    <header></header>
+    <main>
+        <div class="container">
+            <div class="boxDischi">
+                <div class="boxCard"></div>
+                <div class="boxCard"></div>
+                <div class="boxCard"></div>
+                <div class="boxCard"></div>
+                <div class="boxCard"></div>
+                <div class="boxCard"></div>
+                <div class="boxCard"></div>
+                <div class="boxCard"></div>
+                <div class="boxCard"></div>
+                <div class="boxCard"></div>
+
+            </div>
+
+        </div>
+
+    </main>
 
     
 </body>
