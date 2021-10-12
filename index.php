1deb5aade2ba8d7e72b40247094a245642307d0c
Elefly13 <elymara13@gmail.com>
2021-10-12 16:42:46 +0200

    Fine esercizio

--- index.php
+++ index.php
@@ -6,37 +6,36 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
-
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">    
     <link rel="stylesheet" href="css/style.css">
-    <title>Document</title>
+    <title>PHP Dischi</title>
 </head>
 <body>
-<?php 
-    include __DIR__ .'/partial/database.php';
-    include __DIR__ .'/partial/function.php';
-?>
-    <header></header>
+    <?php 
+        include __DIR__ .'/partial/database.php';
+        include __DIR__ .'/partial/function.php';
+    ?>
+    <header>
+        
+    </header>
     <main>
         <div class="container">
             <div class="boxDischi">
-                <div class="boxCard"></div>
-                <div class="boxCard"></div>
-                <div class="boxCard"></div>
-                <div class="boxCard"></div>
-                <div class="boxCard"></div>
-                <div class="boxCard"></div>
-                <div class="boxCard"></div>
-                <div class="boxCard"></div>
-                <div class="boxCard"></div>
-                <div class="boxCard"></div>
-
+                <?php
+                foreach ($db as $key => $value)
+                echo '<div class="boxCard">
+                    <div class="boxImage">
+                        <img src=" ' .$value["poster"] .'" alt="' .$value["title"] .'">
+                    </div>
+                    <div class="boxText">
+                        <h3>'.$value["title"] .'</h3>
+                        <p>'.$value["author"] .'</p>
+                        <p>'.$value["year"] .'</p>
+                    </div>
+                </div>';
+               ?>
             </div>
-
         </div>
-
-    </main>
-
-    
+    </main>    
 </body>
 </html>
